3152b11b98619c80f8984e62be681b9722de6bb4
Gord Pearson <gord.pearson@shopify.com>
2016-06-14 23:05:18 -0400

    Add `return` to try/catch blocks that end a function

--- src/parser.js
+++ src/parser.js
@@ -945,6 +945,8 @@ function lastReturnStatement(nodeList = []) {
       return nodeList;
     } else if (nodeList[lastIndex].type === 'IfStatement') {
       nodeList[lastIndex] = addReturnStatementToIfBlocks(nodeList[lastIndex]);
+    } else if (nodeList[lastIndex].type === 'TryStatement') {
+      nodeList[lastIndex] = addReturnStatementsToTryCatch(nodeList[lastIndex]);
     } else {
       nodeList[lastIndex] =
         b.returnStatement(
@@ -976,11 +978,19 @@ function addReturnStatementToIfBlocks(node) {
 
 function addReturnStatementToBlock(node) {
   const hasReturnStatement = findIndex(node.body, {type: 'ReturnStatement'}) === node.body.length - 1;
-
-  if (hasReturnStatement) {
-    return node;
+  if (!hasReturnStatement) {
+    node.body = lastReturnStatement(node.body);
   }
-  node.body = lastReturnStatement(node.body);
+  return node;
+}
+
+
+function addReturnStatementsToTryCatch(node) {
+  node.block = addReturnStatementToBlock(node.block);
+  if (node.handler) {
+    node.handler.body = addReturnStatementToBlock(node.handler.body);
+  }
+
   return node;
 }
 
