d7b3dfb8744c4f383f9dd11acc55598bc04f1602
Gord Pearson <gord.pearson@shopify.com>
2016-06-18 21:58:19 -0400

    Fix `unless list.includes(foo)`
    Fixes #145

--- src/parser.js
+++ src/parser.js
@@ -846,13 +846,19 @@ function mapSoakedChainToConditions({properties, base}, meta) {
 }
 
 function mapInArrayExpression(node, meta) {
-  return b.memberExpression(
+  let test = b.memberExpression(
     mapExpression(node.array, meta),
     b.callExpression(
       b.identifier('includes'),
       [mapExpression(node.object, meta)]
     )
   );
+
+  if (node.negated) {
+    test = b.unaryExpression('!', test);
+  }
+
+  return test;
 }
 
 function extractAssignStatementsByArguments(nodes) {
